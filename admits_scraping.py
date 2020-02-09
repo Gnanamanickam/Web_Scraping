f995e09d13e78880c2ddd5f788335d54fc44f292
Gnanamanickam A <gnanamanickam1995@gmail.com>
2020-02-09 18:22:24 +0530

    Renamed files
    Renamed files

---
+++ admits_scraping.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import time
+from bs4 import BeautifulSoup
+import scrapy
+from urllib.request import Request,urlopen
+import requests
+from pandas import ExcelWriters
+import os
+from lxml import html
+main_path = 'D:\PycharmProjects\YocketAdmits'
+os.chdir(main_path)
+os.environ["LANG"] = "en_US.UTF-8"
+
+#from selenium.webdriver import Chrome
+# webdriver = "D:\Browserdriver\chromedriver"
+# driver = Chrome(webdriver)
+
+#college_list = ['124-state-university-of-new-york-at-buffalo', '360-university-of-north-carolina-at-charlotte',
+              #  '305-university-of-illinois-at-chicago', '153-university-of-cincinnati']
+login_url = 'https://yocket.in/account/login'
+session_requests = requests.session()
+result = session_requests.get(login_url)
+tree = html.fromstring(result.text)
+authenticity_token = list(set(tree.xpath("//input[@name='_csrfToken']/@value")))[0]
+
+payload = {
+	"username": "gnanamanickam1995@gmail.com",
+	"password": "*********",
+	"csrfmiddlewaretoken": authenticity_token
+}
+
+result = session_requests.post(
+	login_url,
+	data = payload,
+	headers = dict(referer=login_url)
+)
+
+college_list = ['124-state-university-of-new-york-at-buffalo']
+college_summary = pd.DataFrame()
+status = 2
+#print(college_summary)
+for college_index in range(0, len(college_list)):
+    college_name = college_list[college_index]
+
+    for page_no in range(2,3):
+
+
+        original_html = 'https://yocket.in/applications-admits-rejects/'+ college_name + '/' + str(status) + '?page=' + str(page_no)
+        #driver.get(original_html)
+        urlpage = str(original_html)
+       # print(urlpage)
+       #  page = Request.get(urlpage, headers={'User-Agent': 'Mozilla/5.0'}).content();
+       #  webpage = urlopen(page).read()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
+            'Content-Type': 'text/html',
+        }
+        webpage = requests.get(urlpage, headers = headers).text
+        # webpage = driver.get(urlpage)
+        print(webpage)
+        # with urllib.request.urlopen(urlpage) as response:
+        #     html = response.read()
+        #     print(html)
+
+        soup = BeautifulSoup(webpage, 'html.parser')
+        print(webpage)
+        all_gre = []
+        all_gpa = []
+        all_name = []
+
+
+
+        work_exp = []
+        names = soup.find_all('div', attrs={'class': 'col-sm-9'})
+        for i in range(0,len(names)):
+            name = names[i]
+            # print(name)
+
+            # if names['href'].startswith('/players'):
+            # if(names.has_attr('href')):
+        # names = soup.find_all('div', attrs={'class' : 'col-sm-9'})
+        # for name_list in range(0, len(names)):
+        #     name = str(names[name_list].text)
+        #     name = name.strip()
+        #     all_name.append(name)
+        #
+        # print(all_name)
+
+
+
+
+
+
+
+
+
+
--- login_scraper.py
+++ login_scraper.py
@@ -1,31 +1,26 @@
 import requests
 from lxml import html
 
-USERNAME = "<USERNAME>"
-PASSWORD = "<PASSWORD>"
+USERNAME = "gnanamanickam1995@gmail.com"
+PASSWORD = "*******"
 
-LOGIN_URL = "https://bitbucket.org/account/signin/?next=/"
-URL = "https://bitbucket.org/dashboard/repositories"
+LOGIN_URL = ""
+URL = ""
 
 def main():
     session_requests = requests.session()
-
-    # Get login csrf token
     result = session_requests.get(LOGIN_URL)
     tree = html.fromstring(result.text)
     authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
 
-    # Create payload
     payload = {
         "username": USERNAME,
         "password": PASSWORD,
         "csrfmiddlewaretoken": authenticity_token
     }
 
-    # Perform login
     result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
 
-    # Scrape url
     result = session_requests.get(URL, headers = dict(referer = URL))
     tree = html.fromstring(result.content)
     bucket_names = tree.xpath("//div[@class='repo-list--repo']/a/text()")
